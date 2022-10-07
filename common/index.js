99fdedc5282ce78e022beff32c63a731fd0feb69
minhnp <xiuquay55@gmail.com>
2022-10-08 00:45:21 +0700

    fix bug

--- common/index.js
+++ common/index.js
@@ -4,8 +4,6 @@ export * as awsDynamoDB from './services/aws-dynamodb';
 
 export * as dynamoose from './services/dynamoose';
 
-export * as sentryNode from './sentry/sentry-node';
-
 import sentryNode from './sentry/sentry-node';
 import sentryServerless from './sentry/sentry-serverless';
 
